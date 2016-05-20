0b3345f0339723412e4af76c468d9f0108b821e5
followtry <zhongzhijing@126.com>
2016-05-20 15:39:17 +0800

    删除无用笔记

--- _drafts/功能代码/获取时间函数.java
+++
@@ -1,6 +0,0 @@
-//JAVA获得系统时间
-public static String getTime(){
-SimpleDateFormat sdf = new    SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    String time=sdf.format(new java.util.Date());
-    return time;
-}
